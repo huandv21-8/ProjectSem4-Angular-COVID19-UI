714e5fd4d6fa24a1b26b99dcc339154ce5ff2918
vanphuong08 <60741003+vanphuong08@users.noreply.github.com>
2021-09-08 12:59:29 +0700

    8/9

---
+++ src/app/client/confirm-phone-number/confirm-phone-number.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-phone-number',
+  templateUrl: './confirm-phone-number.component.html',
+  styleUrls: ['./confirm-phone-number.component.scss']
+})
+export class ConfirmPhoneNumberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/client/register-phone-number/register-phone-number.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-phone-number',
+  templateUrl: './register-phone-number.component.html',
+  styleUrls: ['./register-phone-number.component.scss']
+})
+export class RegisterPhoneNumberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
